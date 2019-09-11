edcb014a89f4559ed0bbe93cfbc47765001a6140
konst187 <konst-den20@mail.ru>
2019-09-12 00:09:00 +0300

    71 finally

--- test71.py
+++ test71.py
@@ -1,17 +1,21 @@
-file = open('D:\\repo_py\\test_py\\ospf.txt', 'r')
+file = open('C:\\Users\\konst\Desktop\\repo\\ospf.txt', 'r')
 cfg = []
 for i in file:
     cfg += file.readlines()
 print(cfg)
-templ = ['Protocol:', 'Prefix:', 'AD/Metric:', 'Next-Hop:', 'Last update:', 'Outbound Interface']
-lest = []
-for i in cfg:
-    lest[i] = i.split()
-print(lest)
+templ = ['Protocol:', 'Prefix:', 'AD/Metric:', 'Next-Hop:', 'Last update:', 'Outbound Interface', ' ']
 
-''' for line in cfg:
-    for word in line:
-        print(word + '\n') '''
-
-
+for i in range(len(cfg)):
+    cfg[i] = cfg[i].replace("O", "OSPF")
+    cfg[i] = cfg[i].replace("via", "")
+    cfg[i] = cfg[i].split()
+    cfg[i][2] = cfg[i][2].strip("[]")
+    cfg[i][4] = cfg[i][4].strip(",")
+    cfg[i][3] = cfg[i][3].strip(",")
+    
+for i in range(len(cfg)):
+    print("-"*40)
+    for j in range(len(cfg[i])):
+        print(("{0:<20} {1} \n".format(templ[j], cfg[i][j]).rstrip()))
+        
 file.close()
