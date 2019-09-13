dd7fc800a24c47112b9f3fc9c2b0f8bd667ca801
konst187 <konst-den20@mail.ru>
2019-09-13 15:38:01 +0300

    72 imr++

--- test72.py
+++ test72.py
@@ -2,9 +2,10 @@ from sys import argv
 
 ignore = ['duplex', 'alias', 'Current configuration']
 name = argv[1]
+name2 = argv[2]
 print(name)
 file = open(name, "r")
-filer = open("config_sw1_cleared.txt", "w")
+filer = open(name2, "w")
 for i in file:
     if (not (i.startswith("!"))):
         if (i.find(ignore[0]) == -1) and (i.find(ignore[1]) == -1) and (i.find(ignore[2]) == -1): 
